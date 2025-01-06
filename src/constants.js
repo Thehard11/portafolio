bb1f6ba53f569aa6b05f244acd5f5f22f09ec707
Thehard11 <obama111203@gmail.com>
2025-01-06 04:05:49 -0400

    segundo arreglo de links

--- src/constants.js
+++ src/constants.js
@@ -4,7 +4,7 @@ export const dialogueData = {
     pc: `Esta es mi PC aqui es donde me paso la mayoria de mi tiempo haciendo proyectos como este`,
     "cs-degree": `Este es mi titulo del ITLA en Tecnologia en Desarrollo de Softaware`,
     "sofa-table": `Este es el sofa donde me pongo a ver peliculas`,
-    tv: `Aqui es donde veo mis peliculas favoritas aqui tienes una <a href="https://www19.pelisplushd.to/pelicula/guerra-mundial-z">ver</a>`,
+    tv: `Aqui es donde veo mis peliculas favoritas aqui tienes una <a href="https://www.netflix.com/browse">ver</a>`,
     bed: `Simplemente una cama`,
     resume: `Aqui puedes ver mi curriculum <a href="https://github.com/Thehard11/portafolio/blob/main/CV.pdf">ver</a>`,
     projects: `Esta pagina fue hecha en kaboom js es una libreria de js que te permite crear video juegos`,

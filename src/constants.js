e81776141406f5e4cc58cc36f21f7b804d4f59b2
Thehard11 <obama111203@gmail.com>
2025-02-19 00:02:05 -0400

    cambio de constantes

--- src/constants.js
+++ src/constants.js
@@ -1,23 +1,13 @@
 export const scaleFactor=4; 
 
 export const dialogueData = {
-    pc: `This is my PC. I work mostly in JavaScript/TypeScript these days.
-      I've made a couple of games in that language. I also like Golang and Python. Anyway regardless of the language, I just like programming.
-      Here is my <a href="https://github.com/jslegenddev" target="_blank">Github</a>!`,
-    "cs-degree": `This is my CS degree. I hung it on the wall because I'm proud of it. It was a very theoretical degree but I think it gave me a good foundation.`,
-    "sofa-table": `That's my sofa. I like to relax here and watch YouTube. 
-    I also make game programming tutorials on YouTube. Go sub to <a href="https://youtube.com/@jslegenddev" target="_blank">my channel</a>! (If you like the content)
-    You'll learn how I built this portfolio you're currently playing through!`,
-    tv: `That's my TV. I've been watching tech youtubers a lot recently like :
-     <a href="https://www.youtube.com/@ThePrimeTimeagen" target="_blank">Theprimeagen</a>, <a href="https://www.youtube.com/@t3dotgg" target="_blank">Theo - t3.gg</a>,
-    <a href="https://www.youtube.com/@PirateSoftware" target="_blank">PirateSoftware</a> (sometimes) and <a href="https://www.youtube.com/@MelkeyDev" target="_blank">Melkey</a>!`,
-    bed: `This where I sleep. Great ideas comes when I'm lying on my bed. When an idea strikes, I often have to write it down or else I won't be able to sleep because my mental energy is consumed by it.`,
-    resume: `This is my desk and on it is my resume. <a href="https://github.com/JSLegendDev/Resume/blob/main/JSLegend%20Resume-1.pdf" target="_blank">Check it out?</a>
-    Contact me at jslegend@protonmail.com if you have any interesting job opportunities!`,
-    projects: `Info about this portfolio : It's made with the Kaboom.js library which is a library for making games in JavaScript.
-    Text is rendered with HTML/CSS. So the textbox you're currently reading is not rendered within canvas. Learn more about how to use
-    Kaboom.js by watching some of my tutorials <a href="https://youtube.com/@jslegenddev" target="_blank">here</a>.`,
-    library: `There are a lot of programming books on my shelves. There is even one in French (I also speak French btw).
-    I probably only read one of them. Who else compulsively buys technical books without ever finishing them?`,
-    exit: `If you want to exit JSLegendDev's portfolio, just close the tab.`,
+    pc: `Esta es mi PC aqui es donde me paso la mayoria de mi tiempo haciendo proyectos como este`,
+    "cs-degree": `Este es mi titulo del ITLA en Tecnologia en Desarrollo de Softaware`,
+    "sofa-table": `Este es el sofa donde me pongo a ver peliculas`,
+    tv: `Aqui es donde veo mis peliculas favoritas aqui tienes una <a href="https://www.netflix.com/browse">ver</a>`,
+    bed: `Simplemente una cama`,
+    resume: `Aqui puedes ver mi curriculum <a href="https://github.com/Thehard11/portafolio/blob/main/CV.pdf">ver</a>`,
+    projects: `Esta pagina fue hecha en kaboom js es una libreria de js que te permite crear video juegos`,
+    library: `Esta es mi estante de libros un libro que te puedo recomendar seria habitos atomicos`,
+    exit: `Ya te quieres ir? </3`,
   };
